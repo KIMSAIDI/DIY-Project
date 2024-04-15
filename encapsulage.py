a9f9caac29b49d6ce390c629d69ce34659fb3948
KIMSAIDI <kim.saidi15@gmail.com>
2024-04-15 16:27:00 +0200

    commit début de partie 3

---
+++ encapsulage.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class Sequentiel:
+    
+    def __init__(self):
+        self.modules = []
+        
+    def add(self, module):
+        """
+        Ajoute un module à la liste des modules du réseau.
+        
+        Args:
+            module (Module): Module à ajouter.
+        """
+        self.modules.append(module)
+        
+    
+    def forward(self, x):
+        """
+        Calcule la sortie du réseau pour une entrée x.
+        
+        Args:
+            x (numpy.ndarray): Entrée du réseau.
+            
+        Returns:
+            numpy.ndarray : Sortie du réseau.
+        """
+        for module in self.modules:
+            x = module.forward(x)
+        return x
+    
+    def backward(self, x, delta):
+        """
+        Calcule le gradient de la perte par rapport à l'entrée du réseau.
+        
+        Args:
+            x (numpy.ndarray): Entrée du réseau.
+            delta (numpy.ndarray): Gradient de la perte par rapport à la sortie du réseau.
+            
+        Returns:
+            numpy.ndarray : Gradient de la perte par rapport à l'entrée du réseau.
+        """
+        for module in reversed(self.modules):
+            delta = module.backward(x, delta)
+        return delta
+    
+    
+        
+        
