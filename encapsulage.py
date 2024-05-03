e9a5ee03a9131f15ba144c41751ecdc5baf0de7e
KIMSAIDI <kim.saidi15@gmail.com>
2024-05-04 01:14:34 +0200

    début part3

--- encapsulage.py
+++ encapsulage.py
@@ -3,7 +3,7 @@ import numpy as np
 class Sequentiel:
     
     def __init__(self):
-        self.modules = []
+        self.modules = modules # [Module1, Module2, ...]
         
     def add_module(self, module):
         """
@@ -14,35 +14,43 @@ class Sequentiel:
         """
         self.modules.append(module)
         
-    
-    def forward(self, x):
+    def forward_seq(self, input):
         """
-        Calcule la sortie du réseau pour une entrée x.
+        Calcule la sortie du réseau pour une entrée input.
         
         Args:
-            x (numpy.ndarray): Entrée du réseau.
+            input: Entrée du réseau.
             
         Returns:
-            numpy.ndarray : Sortie du réseau.
+            Liste des sorties de chaque module du réseau.
         """
-        for module in self.modules:
-            x = module.forward(x)
-        return x
+        list = []
+        for module in self.modules :
+            list.append(module.forward(input))
+        return list
     
-    def backward(self, x, delta):
+    def backward_delta_seq(self, x, delta):
         """
         Calcule le gradient de la perte par rapport à l'entrée du réseau.
         
         Args:
-            x (numpy.ndarray): Entrée du réseau.
-            delta (numpy.ndarray): Gradient de la perte par rapport à la sortie du réseau.
+            x : Entrée du réseau.
+            delta : Gradient de la perte par rapport à la sortie du réseau.
             
         Returns:
             numpy.ndarray : Gradient de la perte par rapport à l'entrée du réseau.
         """
-        for module in reversed(self.modules):
-            delta = module.backward(x, delta)
-        return delta
+        # A FAIRE
+        pass
+            
+    def update_parameters_seq(self, gradient_step):
+        """ 
+        Met à jour les paramètres de chaque module du réseau.
+        """
+        for module in self.modules : 
+            module.update_parameters(gradient_step)
+            module.zero_grad()
+            
     
     
         
