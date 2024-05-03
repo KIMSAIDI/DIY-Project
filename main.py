334e1fea48a1321bfd39ad1beeb729896c1301e4
KIMSAIDI <kim.saidi15@gmail.com>
2024-05-04 01:16:12 +0200

    delete main

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from classe_lineaire import Linear, MSELoss
-from classe_non_lineaire import TanH, Sigmoid
-from encapsulage import Sequentiel
-
-
-
-# ------------------- Test pour la classe Linear -------------------
